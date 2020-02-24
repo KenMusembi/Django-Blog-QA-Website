78fb433b470f381d7ec38e6ef3975ea45984719e
Kenmusembi <kenmusembi21@gmail.com>
2020-02-24 09:31:41 +0300

    crud for posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),       path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),       path('about/', views.about, name='blog-about'),
 ]
